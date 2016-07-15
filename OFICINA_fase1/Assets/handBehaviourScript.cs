0a0b9ccd0cfa633cf7a5682380a87d5691cb106b
hugofonseca <hugo.mig.28@gmail.com>
2016-07-15 12:19:23 -0300

    1oPROT
    first commit of 1oPROT

---
+++ OFICINA_fase1/Assets/handBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handBehaviourScript : MonoBehaviour {
+
+	float speed = 0.2f;	
+	float jumpspeed = 5;
+	void Update (){
+
+		// moveX
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x -= speed;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x += speed;
+			this.transform.position = position;
+		}
+
+		// jump
+		if(Input.GetKeyDown("space"))
+		{
+			Vector3 position = this.transform.position;
+			position.y += jumpspeed;
+			this.transform.position = position;
+		}
+ }
+}

dc4d06534d7c76c4ba4949f9d8248f99baa1fe67
hugofonseca <hugo.mig.28@gmail.com>
2016-08-01 12:21:09 -0300

    BetterJump
    The player jumps better. Next step is design and program the others
    levels

--- OFICINA_fase1/Assets/handBehaviourScript.cs
+++ OFICINA_fase1/Assets/handBehaviourScript.cs
@@ -3,30 +3,73 @@ using System.Collections;
 
 public class handBehaviourScript : MonoBehaviour {
 
-	float speed = 0.2f;	
-	float jumpspeed = 5;
+	float velocidade;	
+	float animVelocidade;
+	float flip = 0f;
+	Rigidbody2D rb2d;
+	Animator anim;
+
+	bool ground = false;
+
+	void Start()
+	{
+		anim = gameObject.GetComponent<Animator> ();
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	//float jumpspeed;
+	bool isGrounded;
 	void Update (){
 
+		animVelocidade = Input.GetAxis ("Horizontal");
 		// moveX
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			Vector3 position = this.transform.position;
-			position.x -= speed;
+			position.x -= 0.3f;
+			flip = 180f;
 			this.transform.position = position;
 		}
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
+		if (Input.GetKey (KeyCode.RightArrow))
+		    {
 			Vector3 position = this.transform.position;
-			position.x += speed;
+			position.x += 0.3f;
+			flip = 0f;
 			this.transform.position = position;
+
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow) && ground)
+		{
+			rb2d.AddForce(Vector2.up * 1350); // new vector2(0f, 1f);
 		}
 
-		// jump
-		if(Input.GetKeyDown("space"))
+		anim.SetFloat ("velocidade", Mathf.Abs (animVelocidade));
+
+		transform.rotation = Quaternion.Euler (new Vector3 (0f, flip, 0f));
+		Debug.Log (velocidade);
+	}
+	
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag.Equals("ground"))
 		{
-			Vector3 position = this.transform.position;
-			position.y += jumpspeed;
-			this.transform.position = position;
+			ground = true;
 		}
- }
-}
+	}
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if(col.gameObject.tag.Equals("ground"))
+		{
+			ground = true;
+		}
+		else if (col.gameObject.tag.Equals("enemy"))
+		{
+			ground = true;
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
+		ground = false;
+	}
+}	
+	
